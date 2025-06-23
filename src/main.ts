bbcdf224c520b810a085c155457c7bfb653ea8f6
Mamadou Martin <mamadoumartin03@gmail.com>
2025-06-23 20:14:42 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,18 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/auth.interceptor';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/servMAINice-worker';
 
 bootstrapApplication(AppComponent,{
   providers: [
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideRouter(routes)
+    provideRouter(routes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch((err) => console.error(err));
